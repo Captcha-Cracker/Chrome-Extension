8a14f8f7d0e11c06e512d40203b98e53a7f16ec5
ChooSeoYeon <choo000407@naver.com>
2023-05-23 16:43:01 +0900

    Feat: 사용자 지정 캡처 기능

--- popup.js
+++ popup.js
@@ -1,13 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
   const captureButton = document.getElementById("captureButton");
   const imageContainer = document.getElementById("imageContainer");
+  let isCapturing = false;
+  let startX, startY;
 
   captureButton.addEventListener("click", function () {
-    chrome.tabs.captureVisibleTab(function (screenshotUrl) {
-      const image = new Image();
-      image.src = screenshotUrl;
-      imageContainer.innerHTML = "";
-      imageContainer.appendChild(image);
-    });
+    if (!isCapturing) {
+      isCapturing = true;
+      captureButton.textContent = "Stop Capture";
+      document.body.style.cursor = "crosshair";
+    } else {
+      isCapturing = false;
+      captureButton.textContent = "Capture";
+      document.body.style.cursor = "default";
+    }
+  });
+
+  document.addEventListener("mousedown", function (event) {
+    if (isCapturing) {
+      startX = event.clientX;
+      startY = event.clientY;
+    }
+  });
+
+  document.addEventListener("mouseup", function (event) {
+    if (isCapturing) {
+      const endX = event.clientX;
+      const endY = event.clientY;
+
+      const left = Math.min(startX, endX);
+      const top = Math.min(startY, endY);
+      const width = Math.abs(endX - startX);
+      const height = Math.abs(endY - startY);
+
+      chrome.tabs.captureVisibleTab(function (screenshotUrl) {
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+
+        const image = new Image();
+        image.onload = function () {
+          canvas.width = width;
+          canvas.height = height;
+          context.drawImage(image, left, top, width, height, 0, 0, width, height);
+
+          const capturedImageUrl = canvas.toDataURL("image/png");
+          const capturedImage = new Image();
+          capturedImage.src = capturedImageUrl;
+
+          imageContainer.innerHTML = "";
+          imageContainer.appendChild(capturedImage);
+        };
+        image.src = screenshotUrl;
+      });
+
+      isCapturing = false;
+      captureButton.textContent = "Capture";
+      document.body.style.cursor = "default";
+    }
   });
 });
